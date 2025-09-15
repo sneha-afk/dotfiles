dd733e18124050a60c111882a24c4fa851b0395a
Sneha De <55897319+sneha-afk@users.noreply.github.com>
2025-09-15 10:40:36 -0700

    fix: blink.cmp 1.70 resolved performance issues with snippets

--- dot-config/nvim/lua/plugins/completions.lua
+++ dot-config/nvim/lua/plugins/completions.lua
@@ -6,49 +6,6 @@ return {
   event = "InsertEnter",
   dependencies = {
     "rafamadriz/friendly-snippets",
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      opts = {
-        history = true,
-        region_check_events = "InsertEnter",
-        update_events = "TextChanged,TextChangedI",
-        delete_check_events = "TextChanged",
-      },
-      config = function(_, opts)
-        require("luasnip").config.setup(opts)
-
-        require("luasnip.loaders.from_vscode").lazy_load({
-          paths = {
-            vim.fn.stdpath("data") .. "/lazy/friendly-snippets", -- Built-in vscode-style snippets
-            vim.fn.stdpath("config") .. "/snippets",             -- Personal snippets in .config/nvim/snippets
-            vim.uv.cwd() .. "/.nvim/snippets",                   -- Project-specific snippets
-          },
-        })
-
-        vim.api.nvim_create_user_command("EditSnippets", function()
-          require("luasnip.loaders").edit_snippet_files({
-            format = function(file, source_name)
-              local path_replacements = {
-                ["/.local/share/nvim/lazy/friendly%-snippets"] = "FriendlySnippets",
-                ["/.config/nvim/snippets"] = "Personal",
-                ["/dotfiles/dot%-config/nvim/snippets"] = "Personal",
-                [vim.uv.cwd() .. "/.nvim/snippets"] = "Project",
-                ["/.local/share/nvim/lazy/LuaSnip"] = "LuaSnip",
-              }
-
-              for pattern, label in pairs(path_replacements) do
-                if file:find(pattern) then
-                  local filename = file:match(".*/(.*)$") or file
-                  return string.format("%s : %s", label, filename)
-                end
-              end
-              return file
-            end,
-          })
-        end, { desc = "Open snippet files for editing" })
-      end,
-    },
   },
   keys = {
     {
@@ -108,18 +65,8 @@ return {
             insert_command = true,
           },
         },
-        snippets = {
-          opts = {
-            -- Selectively disable snippets by returning false
-            -- filter_snippets = function(ft, file)
-            --   -- 2025-08-31: Excessive lag on Windows only on my own snippets, when using vim.snippet
-            --   return not (vim.g.is_windows and file:find("latex.json"))
-            -- end,
-          },
-        },
       },
     },
-    snippets = { preset = "luasnip" },
     signature = {
       enabled = true,
       window = {
